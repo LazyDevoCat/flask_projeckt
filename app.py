d9cbabcaa18132a32beeb0b4f22e0c7a518e9073
Maria Maksimova <drogzed@gmail.com>
2022-09-12 21:42:25 +0400

    Create update task

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index():
             return redirect('/')
         except:
             return "Issue was added to form"
-        
+    
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
@@ -37,16 +37,28 @@ def index():
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
-    
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
-    
     except:
-        return "Issue was added to form"
-        
+        return "Issue was deleted"
 
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "The task wasn't updated"
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
